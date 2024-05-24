f3bd4cfec75e850b76c223ced4c6dbe5ba22503f
sonia <sonyacore@protonmail.com>
2024-05-24 18:57:48 +0330

    feat(main): added Discount & DiscountTransaction models for auto-migration operation in migrate.go

--- migrate/migrate.go
+++ migrate/migrate.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	log "github.com/sirupsen/logrus"
 	"payment/api/models"
 	"payment/pkg/config"
@@ -8,42 +9,49 @@ import (
 )
 
 func main() {
+	fmt.Println("Payment Service migration tool")
+	logger := log.New()
+	logger.SetFormatter(&log.TextFormatter{})
+
 	configuration, err := config.LoadConfig("configs/config.yml")
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
-	log.Info("Configuration loaded")
+	logger.Info("Configuration loaded")
 
 	dsn := db.LoadDSN(configuration)
 	database, err := db.New(dsn)
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
-	log.Println("Connected to database")
+	logger.Println("Connected to database")
 
 	// Create enum types
 	err = db.CreateEnumType(database, "discount_type", []string{"voucher", "charge"})
 	if err != nil {
-		log.Info(err)
+		logger.Info(err)
 	}
 	err = db.CreateEnumType(database, "transaction_type", []string{"withdrawal", "deposit"})
 	if err != nil {
-		log.Info(err)
+		logger.Info(err)
 	}
 
 	err = db.CreateEnumType(database, "transaction_status", []string{"pending", "completed", "failed"})
 	if err != nil {
-		log.Info(err)
+		logger.Info(err)
 	}
 
 	// Migrate database models
 	err = db.AutoMigrate(database,
 		models.Wallet{},
-		models.Transaction{})
+		models.Transaction{},
+		models.Discount{},
+		models.DiscountTransaction{},
+	)
 
 	if err != nil {
-		log.Fatal(err)
+		logger.Fatal(err)
 	}
 
-	log.Info("Database models migrated")
+	logger.Info("Database models migrated")
 }
